d9270bcf608a641d0214a15614ec7fd57a12efb5
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-03-03 17:08:16 +0900

    Initial Commit

---
+++ Floney/FloneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloneyApp.swift
+//  Floney
+//
+//  Created by 남경민 on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct FloneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
