c32bb08ea5d60997a737c316c63408514bec11e7
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-03-08 19:58:01 +0900

    fix : sign in view

--- Floney/Source/SignIn/SignInView.swift
+++ Floney/Source/SignIn/SignInView.swift
@@ -12,88 +12,96 @@ struct SignInView: View {
     @State var email = ""
     @State var password = ""
     var body: some View {
-        VStack(spacing:50) {
-            Image("logo_floney_signin")
-                
-                .padding(EdgeInsets(top: 128, leading: 0, bottom: 0, trailing: 0))
-            
-            VStack(spacing: 20) {
-                TextField("이메일", text: $email)
-                    .padding()
-                    .font(.pretendardFont(.regular, size: 14))
-                    .foregroundColor(.greyScale7)
-                    .background(Color.greyScale12)
-                    .border(Color.greyScale10)
-                    .cornerRadius(12)
-                    .keyboardType(.emailAddress)
-                
-                TextField("비밀번호", text: $password)
-                    .padding()
-                    .font(.pretendardFont(.regular, size: 14))
-                    .foregroundColor(.greyScale7)
-                    .background(Color.greyScale12)
-                    .border(Color.greyScale10)
-                    .cornerRadius(12)
-                
-                Button("로그인 하기") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                }
-                .padding()
-                .font(.pretendardFont(.bold, size: 14))
-                .foregroundColor(.white)
-                .frame(maxWidth: .infinity)
-                .background(Color.primary1)
-                .cornerRadius(12)
-                
-                HStack(spacing:50) {
-                    VStack {
+        NavigationView {
+            ZStack {
+                VStack(spacing:50) {
+                    Image("logo_floney_signin")
+                        .padding(EdgeInsets(top: 128, leading: 0, bottom: 0, trailing: 0))
+                    
+                    VStack(spacing: 20) {
+                        TextField("", text: $email)
+                            .padding()
+                            .keyboardType(.emailAddress)
+                            .overlay(
+                                Text("이메일")
+                                    .padding()
+                                    .font(.pretendardFont(.regular, size: 14))
+                                    .foregroundColor(.greyScale7)
+                                    .opacity(email.isEmpty ? 1 : 0), alignment: .leading
+                            )
+                            .modifier(TextFieldModifier())
                         
-                        Button("비밀번호 찾기") {
+                        
+                        TextField("", text: $password)
+                            .padding()
+                            .overlay(
+                                Text("비밀번호")
+                                    .padding()
+                                    .font(.pretendardFont(.regular, size: 14))
+                                    .foregroundColor(.greyScale7)
+                                    .opacity(password.isEmpty ? 1 : 0), alignment: .leading
+                            )
+                            .modifier(TextFieldModifier())
+                        
+                        Text("로그인 하기")
+                            .padding()
+                            .modifier(NextButtonModifier())
+                        
+                        HStack(spacing:50) {
+                            VStack {
+                                Text("비밀번호 찾기")
+                                    .font(.pretendardFont(.regular, size: 12))
+                                    .foregroundColor(.greyScale6)
+                                
+                                
+                                Divider()
+                                    .padding(EdgeInsets(top: -10, leading: 0, bottom: 0, trailing: 0))
+                                    .frame(width: 70,height: 1.0)
+                                    .foregroundColor(.greyScale6)
+                            }
                             
+                            NavigationLink(destination: ServiceAgreementView()){
+                                VStack {
+                                    Text("회원가입 하기")
+                                        .font(.pretendardFont(.regular, size: 12))
+                                        .foregroundColor(.greyScale6)
+                                    Divider()
+                                        .padding(EdgeInsets(top: -10, leading: 0, bottom: 0, trailing: 0))
+                                        .frame(width: 70,height: 1.0)
+                                        .foregroundColor(.greyScale6)
+                                    
+                                }
+                            }
                         }
-                        .font(.pretendardFont(.regular, size: 12))
-                        .foregroundColor(.greyScale6)
-                        
-                        
-                        Divider()
-                            .padding(EdgeInsets(top: -10, leading: 0, bottom: 0, trailing: 0))
-                            .frame(width: 70,height: 1.0)
-                            .foregroundColor(.greyScale6)
                         
                     }
-                    
-                    VStack {
-                        Button("회원가입 하기") {
-                            
+                    .padding(20)
+                    VStack(spacing:20) {
+                        Text("간편 로그인")
+                            .font(.pretendardFont(.medium,size: 12))
+                        HStack(spacing:30) {
+                            Image("btn_kakao")
+                            Image("btn_google")
+                            Image("btn_apple")
                         }
-                        
-                        .font(.pretendardFont(.regular, size: 12))
-                        .foregroundColor(.greyScale6)
-                        Divider()
-                            .padding(EdgeInsets(top: -10, leading: 0, bottom: 0, trailing: 0))
-                            .frame(width: 70,height: 1.0)
-                            .foregroundColor(.greyScale6)
-                        
                     }
+                    Spacer()
                 }
             }
-            .padding(20)
-            VStack(spacing:20) {
-                Text("간편 로그인")
-                    .font(.pretendardFont(.medium,size: 12))
-                HStack(spacing:30) {
-                    Image("kakao_signin")
-                    Image("apple_signin")
-                    Image("google_signin")
-                    
+            .onTapGesture {
+                // Hide the keyboard
+                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+            }
+            .onAppear {
+                // Set the TextField as the first responder
+                DispatchQueue.main.async {
+                    UIApplication.shared.windows.first?.rootViewController?.view.endEditing(false)
                 }
             }
-            Spacer()
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .onAppear(perform : UIApplication.shared.hideKeyboard)
     }
 }
+
 
 struct SignInView_Previews: PreviewProvider {
     static var previews: some View {
