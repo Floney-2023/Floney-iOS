68573595f65d7b43fd9fe1a6f813edc664980c4c
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-03-12 22:51:02 +0900

    feat : home calendar

--- Floney/Source/Main/Home/HomeView.swift
+++ Floney/Source/Main/Home/HomeView.swift
@@ -4,14 +4,109 @@
 //
 //  Created by 남경민 on 2023/03/09.
 //
-
 import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.background2
+            VStack {
+                HStack
+                {
+                    Image("logo_floney_home")
+                    Spacer()
+                    Image("icon_profile")
+                }
+                CalendarView()
+            }
+            .padding(20)
+        }
+        .edgesIgnoringSafeArea(.vertical)
+        
     }
 }
+
+struct CalendarView: View {
+    @State private var selectedDate = Date()
+    
+    private var calendar: Calendar {
+        var calendar = Calendar.current
+        calendar.locale = Locale.current
+        return calendar
+    }
+    
+    // 일,월,화,수,목,금,토
+    private var weekdays: [String] {
+        let weekdays = calendar.shortWeekdaySymbols
+        let firstWeekdayIndex = calendar.firstWeekday - 1
+        return Array(weekdays[firstWeekdayIndex..<weekdays.count] + weekdays[0..<firstWeekdayIndex])
+    }
+    
+    // 달
+    private var month: DateComponents {
+        return calendar.dateComponents([.year, .month], from: selectedDate)
+    }
+    
+    // 해당 달의 시작날
+    private var monthStartDate: Date {
+        return calendar.date(from: month)!
+    }
+    
+    // 해당 달의 마지막날
+    private var monthEndDate: Date {
+        return calendar.date(byAdding: DateComponents(month: 1, day: -1), to: monthStartDate)!
+    }
+    
+    // 하루 하루 2차 배열
+    private var weeks: [[Date]] {
+        let startDate = calendar.date(from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: monthStartDate))!
+        let endDate = calendar.date(from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: monthEndDate))!
+        var weeks: [[Date]] = []
+        var week: [Date] = []
+        var date = startDate
+        while date <= endDate {
+            week.append(date)
+            if calendar.isDate(date, equalTo: endDate, toGranularity: .weekOfYear) {
+                weeks.append(week)
+            }
+            if calendar.component(.weekday, from: date) == calendar.firstWeekday {
+                weeks.append(week)
+                week = []
+            }
+            date = calendar.date(byAdding: .day, value: 1, to: date)!
+        }
+        return weeks
+    }
+    
+    var body: some View {
+        VStack {
+            Text("\(month.month!)")
+                .font(.pretendardFont(.semiBold, size: 20))
+            
+            HStack(spacing: 0) {
+                ForEach(weekdays, id: \.self) { weekday in
+                    Text(weekday)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            
+            
+            ForEach(weeks, id: \.self) { week in
+                HStack(spacing: 0) {
+                    ForEach(week, id: \.self) { date in
+                        Text(calendar.component(.day, from: date) == 1 ? "\(calendar.component(.month, from: date))/\(calendar.component(.day, from: date))" : "\(calendar.component(.day, from: date))")
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(calendar.isDate(date, inSameDayAs: selectedDate) ? Color.primary1 : Color.clear)
+                            .onTapGesture {
+                                selectedDate = date
+                            }
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
--- Floney/Source/Main/MainTabView.swift
+++ Floney/Source/Main/MainTabView.swift
@@ -9,34 +9,31 @@ import SwiftUI
 
 struct MainTabView: View {
     @State var selection = 0
+    init() {
+    UITabBar.appearance().backgroundColor = UIColor.white
+
+    }
     
     var body: some View {
         TabView(selection: $selection) {
             HomeView().tabItem {
                 if selection == 0 {
                     Image("icon_home_on")
-                    Text("홈")
-                        .font(.pretendardFont(.regular, size: 10))
-                        .foregroundColor(.greyScale2)
                 } else {
                     Image("icon_home_off")
-                    Text("홈")
-                        .foregroundColor(.greyScale7)
                 }
-                
+                Text("홈")
             }.tag(0)
+                
+
             
             AnalysisView().tabItem {
                 if selection == 1 {
                     Image("icon_leaderboard_on")
-                    Text("분석")
-                        .foregroundColor(.greyScale2)
                 } else {
                     Image("icon_leaderboard_off")
-                    Text("분석")
-                        .foregroundColor(.greyScale7)
                 }
-                
+                Text("분석")
             }.tag(1)
             
             AddView().tabItem {
@@ -46,28 +43,36 @@ struct MainTabView: View {
             CalculateView().tabItem {
                 if selection == 3 {
                     Image("icon_calculate_on")
-                    Text("정산")
-                        .foregroundColor(.greyScale2)
                 } else {
                     Image("icon_calculate_off")
-                    Text("정산")
-                        .foregroundColor(.greyScale7)
                 }
+                Text("정산")
                 
             }.tag(3)
             
             MyPageView().tabItem {
                 if selection == 4 {
                     Image("icon_person_on")
-                    Text("마이")
-                        .foregroundColor(.greyScale2)
                 } else {
                     Image("icon_person_off")
-                    Text("마이")
-                        .foregroundColor(.greyScale7)
                 }
+                Text("마이")
             }.tag(4)
+            
         }
+        .accentColor(.greyScale2)
+    }
+}
+extension UITabBarController {
+    override open func viewDidLoad() {
+        let standardAppearance = UITabBarAppearance()
+        
+        standardAppearance.stackedItemPositioning = .centered
+        standardAppearance.stackedItemSpacing = 44
+        standardAppearance.stackedItemWidth = 60
+        
+      
+        tabBar.standardAppearance = standardAppearance
     }
 }
 
