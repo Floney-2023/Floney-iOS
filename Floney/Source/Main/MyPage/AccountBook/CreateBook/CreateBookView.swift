482604ff4ba7ceca6cf41e33dc9663a3099fc607
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-04-21 23:52:05 +0900

    fix : create book view

--- Floney/Source/Main/MyPage/AccountBook/CreateBook/CreateBookView.swift
+++ Floney/Source/Main/MyPage/AccountBook/CreateBook/CreateBookView.swift
@@ -48,6 +48,7 @@ struct CreateBookView: View {
                         .withNextButtonFormmating(.primary9)
                         .onTapGesture {
                             self.isShowingBottomSheet.toggle()
+                            
                         }
                 }
             }
