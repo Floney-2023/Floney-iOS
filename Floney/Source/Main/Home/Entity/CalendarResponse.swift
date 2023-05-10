1de88cf67a9c4223ca403e2e639c521c64378913
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-05-10 23:18:14 +0900

    fix : calendar

---
+++ Floney/Source/Main/Home/Entity/CalendarResponse.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarResponse.swift
+//  Floney
+//
+//  Created by 남경민 on 2023/05/09.
+//
+
+import Foundation
+
+struct CalendarResponse: Decodable {
+    var totalIncome: Int
+    var totalOutcome: Int
+    var result: CalendarExpenses?
+}
+
+struct CalendarExpenses: Decodable {
+    var date: String
+    var money: Int
+    var assetType: String
+}
