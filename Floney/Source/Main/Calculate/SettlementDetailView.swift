df63c3ee65d3fbbfdc75021eeb5da0b5508053bb
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-07-18 06:09:03 +0900

    feat: settlement detail

---
+++ Floney/Source/Main/Calculate/SettlementDetailView.swift
@@ -0,0 +1,148 @@
+//
+//  SettlementDetailView.swift
+//  Floney
+//
+//  Created by 남경민 on 2023/07/18.
+//
+
+import SwiftUI
+
+struct SettlementDetailView: View {
+    @Binding var isShowingTabbar : Bool
+    @ObservedObject var viewModel : CalculateViewModel
+    var body: some View {
+        VStack {
+            HStack {
+                Image("back_button")
+                
+                Spacer()
+                
+            }
+            .padding(.vertical, 22)
+            .padding(.leading, 20)
+            
+            VStack(alignment: .leading,spacing: 32) {
+                HStack {
+                    VStack(alignment: .leading, spacing: 12) {
+                        Text("2022.10.10 - 10.25")
+                            .foregroundColor(.greyScale7)
+                            .font(.pretendardFont(.medium, size: 14))
+
+                        Text("정산 영수증")
+                            .font(.pretendardFont(.bold, size: 22))
+                            .foregroundColor(.greyScale1)
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal,24)
+    
+                HStack {
+                    Text("총 지출")
+                        .font(.pretendardFont(.medium, size: 16))
+                        .foregroundColor(.greyScale2)
+                    Spacer()
+                    Text("\(viewModel.totalOutcome)원")
+                        .font(.pretendardFont(.bold, size: 16))
+                        .foregroundColor(.greyScale2)
+                }.padding(.horizontal, 28)
+                
+                GeometryReader { geometry in
+                    Path { path in
+                        path.move(to: CGPoint(x: 0, y: 0))
+                        path.addLine(to: CGPoint(x: geometry.size.width, y: 0))
+                    }
+                    .stroke(style: StrokeStyle(lineWidth: 1, dash: [5, 5]))
+                    .foregroundColor(.greyScale9)
+                }
+                .frame(height: 1)
+                .padding(.horizontal,24)
+                VStack(spacing: 20) {
+                    ForEach(viewModel.details, id:\.self){ detail in
+                        HStack {
+                            Text("\(detail.userNickname)")
+                                .font(.pretendardFont(.medium, size: 14))
+                                .foregroundColor(.greyScale6)
+                            Spacer()
+                            Text("\(detail.money + viewModel.outcomePerUser)")
+                                .font(.pretendardFont(.medium, size: 14))
+                                .foregroundColor(.greyScale2)
+                        }
+                    }
+                }.padding(.horizontal, 28)
+                GeometryReader { geometry in
+                    Path { path in
+                        path.move(to: CGPoint(x: 0, y: 0))
+                        path.addLine(to: CGPoint(x: geometry.size.width, y: 0))
+                    }
+                    .stroke(style: StrokeStyle(lineWidth: 1, dash: [5, 5]))
+                    .foregroundColor(.greyScale9)
+                }
+                .frame(height: 1)
+                .padding(.horizontal,24)
+                HStack {
+                    Text("정산 금액")
+                        .font(.pretendardFont(.medium, size: 16))
+                        .foregroundColor(.greyScale2)
+                    Spacer()
+                    Text("1인")
+                        .font(.pretendardFont(.semiBold, size: 10))
+                        .foregroundColor(.greyScale6)
+                    Text("\(viewModel.outcomePerUser)원")
+                        .font(.pretendardFont(.bold, size: 16))
+                        .foregroundColor(.primary2)
+                }
+                .padding(.horizontal,28)
+                
+                
+                Divider()
+                    .frame(height: 4)
+                    .background(Color.background2)
+                    
+                    ForEach(viewModel.details, id: \.self) { detail in
+                        HStack{
+                            Image("icon_profile")
+                            VStack(alignment:.leading) {
+                                Text("\(detail.userNickname)")
+                                    .font(.pretendardFont(.medium, size: 12))
+                                    .foregroundColor(.greyScale2)
+                                if detail.money != 0 {
+                                    Text("\(abs(detail.money))")
+                                        .font(.pretendardFont(.bold, size: 16))
+                                        .foregroundColor(.greyScale2)
+                                    +
+                                    Text(detail.money > 0 ? " 을 받아야해요." : " 을 보내야해요")
+                                        .font(.pretendardFont(.regular, size: 16))
+                                        .foregroundColor(.greyScale2)
+                                } else {
+                                    Text("정산할 금액이 없어요.")
+                                        .font(.pretendardFont(.regular, size: 16))
+                                        .foregroundColor(.greyScale2)
+                                }
+                            }
+                        }
+                    }
+       
+             Spacer()
+            }
+            //.padding(EdgeInsets(top: 32, leading: 24, bottom: 0, trailing: 24))
+            HStack(spacing:0){
+
+                Text("공유하기")
+                    .padding()
+                    .font(.pretendardFont(.bold, size: 14))
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    
+                    .background(Color.primary1)
+            }
+            
+        }.navigationBarBackButtonHidden()
+
+    }
+}
+
+struct SettlementDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettlementDetailView(isShowingTabbar: .constant(false), viewModel: CalculateViewModel())
+    }
+}
--- Floney/Source/Main/Calculate/SettlementListView.swift
+++ Floney/Source/Main/Calculate/SettlementListView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SettlementListView: View {
     @Binding var isShowing : Bool
     @Binding var showingTabbar : Bool
+    @State var showingDetail = false
     @StateObject var viewModel = CalculateViewModel()
     var body: some View {
         VStack(spacing:28) {
@@ -25,74 +26,89 @@ struct SettlementListView: View {
                         isShowing = false
                     }
             }.padding(.bottom,6)
-            ScrollView(showsIndicators: false) {
-                ForEach(0..<1, id:\.self) { list in
-                    VStack(spacing: 12) {
-                        VStack(spacing:22) {
+            /*
+            if viewModel.settlementList.count == 0 {
+                VStack {
+                    Image("no_line")
+                    Text("정산 내역이 없습니다.")
+                        .font(.pretendardFont(.medium,size: 12))
+                        .foregroundColor(.greyScale6)
+                }.padding(.bottom,100)
+                .frame(maxWidth: .infinity)
+                .frame(maxHeight: .infinity)
+            } else {*/
+                ScrollView(showsIndicators: false) {
+                    ForEach(0..<1, id:\.self) { list in
+                        VStack(spacing: 12) {
+                            VStack(spacing:22) {
+                                HStack {
+                                    Text("기간")
+                                        .font(.pretendardFont(.medium,size: 13))
+                                        .foregroundColor(.greyScale6)
+                                    Spacer()
+                                    Text("2022.10.10 - 10.25")
+                                        .font(.pretendardFont(.medium,size: 13))
+                                        .foregroundColor(.greyScale2)
+                                    
+                                }
+                                HStack {
+                                    Text("총 금액")
+                                        .font(.pretendardFont(.medium,size: 13))
+                                        .foregroundColor(.greyScale6)
+                                    Spacer()
+                                    Text("300,000원")
+                                        .font(.pretendardFont(.medium,size: 13))
+                                        .foregroundColor(.greyScale2)
+                                    
+                                }
+                                HStack {
+                                    Text("인원")
+                                        .font(.pretendardFont(.medium,size: 13))
+                                        .foregroundColor(.greyScale6)
+                                    Spacer()
+                                    Text("2명")
+                                        .font(.pretendardFont(.medium,size: 13))
+                                        .foregroundColor(.greyScale2)
+                                    
+                                }
+                            }
+                            .padding(20)
+                            .background(Color.greyScale12)
+                            .cornerRadius(12)
                             HStack {
-                                Text("기간")
-                                    .font(.pretendardFont(.medium,size: 13))
-                                    .foregroundColor(.greyScale6)
-                                Spacer()
-                                Text("2022.10.10 - 10.25")
-                                    .font(.pretendardFont(.medium,size: 13))
+                                Text("정산 금액")
+                                    .font(.pretendardFont(.semiBold,size: 13))
                                     .foregroundColor(.greyScale2)
+                                Spacer()
+                                Text("1인")
+                                    .font(.pretendardFont(.semiBold, size: 10))
+                                    .foregroundColor(.greyScale6)
+                                Text("150,000원")
+                                    .font(.pretendardFont(.bold, size: 16))
+                                    .foregroundColor(.primary2)
+                                Image("forward_button")
+                                    .padding(.leading, 8)
                                 
                             }
-                            HStack {
-                                Text("총 금액")
-                                    .font(.pretendardFont(.medium,size: 13))
-                                    .foregroundColor(.greyScale6)
-                                Spacer()
-                                Text("300,000원")
-                                    .font(.pretendardFont(.medium,size: 13))
-                                    .foregroundColor(.greyScale2)
-                                
-                            }
-                            HStack {
-                                Text("인원")
-                                    .font(.pretendardFont(.medium,size: 13))
-                                    .foregroundColor(.greyScale6)
-                                Spacer()
-                                Text("2명")
-                                    .font(.pretendardFont(.medium,size: 13))
-                                    .foregroundColor(.greyScale2)
-                                
-                            }
+                            .padding(20)
+                            .background(Color.primary10)
+                            .cornerRadius(12)
+                        }.onTapGesture {
+                            showingDetail = true
                         }
-                        .padding(20)
-                        .background(Color.greyScale12)
-                        .cornerRadius(12)
-                        HStack {
-                            Text("정산 금액")
-                                .font(.pretendardFont(.semiBold,size: 13))
-                                .foregroundColor(.greyScale2)
-                            Spacer()
-                            Text("1인")
-                                .font(.pretendardFont(.semiBold, size: 10))
-                                .foregroundColor(.greyScale6)
-                            Text("150,000원")
-                                .font(.pretendardFont(.bold, size: 16))
-                                .foregroundColor(.primary2)
-                            Image("forward_button")
-                                .padding(.leading, 8)
-                            
-                        }
-                        .padding(20)
-                        .background(Color.primary10)
-                        .cornerRadius(12)
                     }
                 }
-            }
+           // }
         
         }
         .padding(.horizontal, 20)
         .onAppear{
             showingTabbar = false
-            viewModel.getSettlementList()
+            //viewModel.getSettlementList()
         }
         .navigationBarBackButtonHidden()
         
+       // NavigationLink(destina)
     }
 }
 
