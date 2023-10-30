48a9b65a74760de0c5077ab68ac876d951cbec1d
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-10-31 01:29:05 +0900

    fix : scaler on change password view

--- Floney/Source/Main/MyPage/UserInformation/ChangePassword/ChangePasswordView.swift
+++ Floney/Source/Main/MyPage/UserInformation/ChangePassword/ChangePasswordView.swift
@@ -8,46 +8,50 @@
 import SwiftUI
 
 struct ChangePasswordView: View {
+    let scaler = Scaler.shared
     @StateObject var viewModel = MyPageViewModel()
     var body: some View {
         ZStack {
-            VStack(spacing:30) {
-                VStack(spacing:12) {
+            VStack(spacing:scaler.scaleHeight(30)) {
+                VStack(spacing:scaler.scaleHeight(12)) {
                     HStack {
                         Text("현재 비밀번호")
-                            .font(.pretendardFont(.medium, size: 14))
+                            .font(.pretendardFont(.medium, size:scaler.scaleWidth(14)))
                             .foregroundColor(.greyScale2)
+                            .padding(.leading, scaler.scaleWidth(4))
                         Spacer()
                     }
                     CustomTextField(text: $viewModel.currentPassword, placeholder: "현재 비밀번호를 입력하세요.",placeholderColor: .greyScale7)
-                        .frame(height: UIScreen.main.bounds.height * 0.06)
+                        .frame(height: scaler.scaleHeight(46))
                 }
                 
-                VStack(spacing: 20) {
-                    VStack(spacing: 12) {
+                VStack(spacing: scaler.scaleHeight(20)) {
+                    VStack(spacing: scaler.scaleHeight(12)) {
                         HStack {
                             Text("새 비밀번호")
-                                .font(.pretendardFont(.medium, size: 14))
+                                .font(.pretendardFont(.medium, size: scaler.scaleWidth(14)))
                                 .foregroundColor(.greyScale2)
+                                .padding(.leading, scaler.scaleWidth(4))
                             Spacer()
                         }
                         CustomTextField(text: $viewModel.newPassword, placeholder: "영문 대소문자, 숫자 포함 8자 이상",placeholderColor: .greyScale7)
-                            .frame(height: UIScreen.main.bounds.height * 0.06)
+                            .frame(height: scaler.scaleHeight(46))
                     }
-                    VStack(spacing: 12) {
+                    VStack(spacing: scaler.scaleHeight(12)) {
                         HStack {
                             Text("새 비밀번호 확인")
-                                .font(.pretendardFont(.medium, size: 14))
+                                .font(.pretendardFont(.medium, size: scaler.scaleWidth(14)))
+                                .padding(.leading, scaler.scaleWidth(4))
                                 .foregroundColor(.greyScale2)
                             Spacer()
                         }
                         CustomTextField(text: $viewModel.newPasswordCheck, placeholder: "영문 대소문자, 숫자 포함 8자 이상",placeholderColor: .greyScale7)
-                            .frame(height: UIScreen.main.bounds.height * 0.06)
+                            .frame(height: scaler.scaleHeight(46))
                     }
                 }
                 
                 Text("확인")
-                    .padding()
+                    .padding(scaler.scaleWidth(20))
                     .withNextButtonFormmating(.primary1)
                     .onTapGesture {
                         if viewModel.isValidInputs() {
@@ -57,11 +61,11 @@ struct ChangePasswordView: View {
                 Spacer()
                 
             }
-            .padding(EdgeInsets(top: 35, leading: 20, bottom: 0, trailing: 20))
+            .padding(EdgeInsets(top: scaler.scaleHeight(35), leading: scaler.scaleWidth(20), bottom: 0, trailing: scaler.scaleWidth(20)))
             .customNavigationBar(
                 leftView: { BackButtonBlack() },
                 centerView: { Text("비밀번호 변경")
-                        .font(.pretendardFont(.semiBold, size: 16))
+                        .font(.pretendardFont(.semiBold, size: scaler.scaleWidth(16)))
                     .foregroundColor(.greyScale1)}
                 
                 )
