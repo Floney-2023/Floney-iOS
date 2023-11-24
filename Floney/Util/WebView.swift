a515056c27e672829e14e541ebe436852b55fe62
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-11-25 00:25:06 +0900

    feat : custom webview

--- Floney/Util/WebView.swift
+++ Floney/Util/WebView.swift
@@ -5,4 +5,20 @@
 //  Created by 남경민 on 11/25/23.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let urlString: String
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+}
