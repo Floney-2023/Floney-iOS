a2751f0c5f8e9211d109bae0ca7b37b7775590b8
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-12-02 01:16:47 +0900

    fix : the loading of settlement view

--- Floney/Source/Main/Calculate/SetContentCalcView.swift
+++ Floney/Source/Main/Calculate/SetContentCalcView.swift
@@ -147,7 +147,7 @@ struct SetContentCalcView: View {
                             } else {
                                 viewModel.postSettlements()
                                 LoadingManager.shared.update(showLoading: true, loadingType: .floneyLoading)
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                                     LoadingManager.shared.update(showLoading: false, loadingType: .floneyLoading)
                                     pageCount = 4
                                 }
