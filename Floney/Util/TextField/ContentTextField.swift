4c6f8fa62ebc2bfe39437154b9b1f161383b271b
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-12-21 01:33:27 +0900

    fix : dynamic font type

--- Floney/Util/TextField/ContentTextField.swift
+++ Floney/Util/TextField/ContentTextField.swift
@@ -12,33 +12,43 @@ struct ContentTextField: UIViewRepresentable {
     let scaler = Scaler.shared
     @Binding var text: String
     var placeholder: String
-    
     var keyboardType: UIKeyboardType = .default
     var alignment: NSTextAlignment = .right  // 새로운 정렬 속성 추가
-    
+    /*
     var textFont: UIFont {
         .pretendardFont(.medium, size:scaler.scaleWidth(14))
+    }*/
+    var textFont: UIFont {
+        createDynamicTypeFont(name: "Pretendard-Medium", baseSize: 14)
     }
-    var textColor: UIColor = .greyScale2
     
     var placeholderFont: UIFont {
-        .pretendardFont(.medium, size:scaler.scaleWidth(14))
+        createDynamicTypeFont(name: "Pretendard-Medium", baseSize: 14)
     }
+    var textColor: UIColor = .greyScale2
+    /*
+    var placeholderFont: UIFont {
+        .pretendardFont(.medium, size:scaler.scaleWidth(14))
+    }*/
     var placeholderColor: UIColor = .greyScale6
     
     var backgroundColor: UIColor = .clear
-    
-    
+    // 커스텀 폰트와 UIFontMetrics를 사용하여 Dynamic Type을 지원하는 폰트 생성
+    private func createDynamicTypeFont(name: String, baseSize: CGFloat) -> UIFont {
+        let scaledSize = scaler.scaleWidth(baseSize)
+        guard let font = UIFont(name: name, size: scaledSize) else {
+            fatalError("Failed to load the \(name) font.")
+        }
+        return UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
+    }
     func makeUIView(context: Context) -> UITextField {
         let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false // Auto Layout 사용 설정
         textField.delegate = context.coordinator
-        
-        
         let attributes = [
             NSAttributedString.Key.foregroundColor: placeholderColor,
             NSAttributedString.Key.font: placeholderFont
         ]
-        
         textField.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: attributes)
         textField.font = textFont
         textField.textColor = textColor
@@ -55,23 +65,19 @@ struct ContentTextField: UIViewRepresentable {
         uiView.text = text
         uiView.placeholder = placeholder
         uiView.backgroundColor = backgroundColor
-        
     }
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-    
     class Coordinator: NSObject, UITextFieldDelegate {
         var parent: ContentTextField
         
         init(_ parent: ContentTextField) {
             self.parent = parent
         }
-        
         func textFieldDidChangeSelection(_ textField: UITextField) {
             parent.text = textField.text ?? ""
-            
         }
     }
 }
