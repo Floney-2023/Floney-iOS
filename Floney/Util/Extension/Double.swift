44996757a15b412d2adaba64c3ecba8e7356af5d
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-12-26 20:45:14 +0900

    fix : analysis-asset, carried over with money type

--- Floney/Util/Extension/Double.swift
+++ Floney/Util/Extension/Double.swift
@@ -17,6 +17,8 @@ extension Double {
         if floor(self) == self {
             if self > 99999999999 {
                 return formatter.string(from: NSNumber(value: 99999999999))!//"99999999999"
+            } else if self < -99999999999 {
+                return formatter.string(from: NSNumber(value: -99999999999))!//"-99999999999"
             } else {
                 return formatter.string(from: number)!//"\(Int(self))"
             }
@@ -25,22 +27,29 @@ extension Double {
         else if floor(self * 10) == self * 10 {
             if self > 999999999.99 {
                 return formatter.string(from: NSNumber(value: 999999999.99))!//"999999999.99"
+            } else if self < -999999999.99 {
+                return formatter.string(from: NSNumber(value: -999999999.99))!//"-999999999.99"
             } else {
                 return formatter.string(from: number)!//String(format: "%.1f", self)
             }
         }
         // 소수점 셋째 자리 이상
+        
         else {
             if CurrencyManager.shared.hasDecimalPoint {
                 if self > 999999999.99 {
                     return formatter.string(from: NSNumber(value: 999999999.99))!//"999999999.99"
-                } else {
+                } else if self < -999999999.99 {
+                    return formatter.string(from: NSNumber(value: -999999999.99))!//"999999999.99"
+                }else {
                     return formatter.string(from: number)!//String(format: "%.2f", self)
                 }
             } else {
                 if self > 99999999999 {
                     return formatter.string(from: NSNumber(value: 99999999999))!//"99999999999"
-                } else {
+                } else if self < -99999999999 {
+                    return formatter.string(from: NSNumber(value: -99999999999))!//"99999999999"
+                }else {
                     return formatter.string(from: number)!//"\(Int(self))"
                 }
             }
