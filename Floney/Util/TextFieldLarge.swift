ebbe4f60064c957505d201301e65aeb9d38c4f07
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2024-01-24 13:31:37 +0900

    #23 fix: 최대 입력 자리 제한

--- Floney/Util/TextFieldLarge.swift
+++ Floney/Util/TextFieldLarge.swift
@@ -21,6 +21,7 @@ struct TextFieldLarge: View {
             }
             .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
     }
+    /*
     func validateAndFormatAmount(budget: String) -> String {
         // 숫자와 소수점만 허용
         let filteredAmount = budget.filter { "0123456789.".contains($0) }
@@ -30,13 +31,13 @@ struct TextFieldLarge: View {
 
         // 최대 금액 검증
         if CurrencyManager.shared.hasDecimalPoint {
-            guard let value = Double(filteredAmount), value <= 999_999_999.99 else {
+            guard let value = Double(filteredAmount), filteredAmount.count > 11 else {
                 return ""
             }
             return formatAmount(value)
         
         } else {
-            guard let value = Int(filteredAmount), value <= 99_999_999_999 else {
+            guard let value = Int(filteredAmount), filteredAmount.count > 11 else {
                 return ""
             }
             return formatAmount(value)
@@ -56,6 +57,42 @@ struct TextFieldLarge: View {
             // 적절한 숫자 타입이 아닌 경우
             return ""
         }
+    }*/
+    
+    func validateAndFormatAmount(budget: String) -> String {
+        // 숫자와 소수점만 허용
+        let filteredAmount = budget.filter { "0123456789.".contains($0) }
+        
+        if (filteredAmount.first == "0" && filteredAmount.count > 1) {
+            return ""
+        }
+        // 11자리를 초과하는 경우, 앞에서부터 11자리만 취함
+        let trimmedAmount = String(filteredAmount.prefix(11))
+
+        // CurrencyManager에 따라 Double 또는 Int로 처리
+        if CurrencyManager.shared.hasDecimalPoint {
+            guard let value = Double(trimmedAmount) else { return "" }
+            return formatAmount(value)
+        } else {
+            guard let value = Int(trimmedAmount) else { return "" }
+            return formatAmount(value)
+        }
     }
+
+    func formatAmount(_ value: Any) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = CurrencyManager.shared.hasDecimalPoint ? 0 : 2
+
+        if let doubleValue = value as? Double {
+            return numberFormatter.string(from: NSNumber(value: doubleValue)) ?? ""
+        } else if let intValue = value as? Int {
+            return numberFormatter.string(from: NSNumber(value: intValue)) ?? ""
+        } else {
+            return ""
+        }
+    }
+     
+     
 }
 
