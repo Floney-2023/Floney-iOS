d9f46c1b28b076f456596a599d6c86a355c6acfc
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2024-02-21 16:30:43 +0900

    #32 feat: 전면 광고 추가

--- Floney/Util/AdCoordinator.swift
+++ Floney/Util/AdCoordinator.swift
@@ -9,80 +9,6 @@ import SwiftUI
 import UIKit
 import GoogleMobileAds
 
-struct GADInterstitialView: UIViewControllerRepresentable {
-    @Environment(\.presentationMode) var presentationMode
-    var adUnitID: String = "ca-app-pub-3940256099942544/4411468910"
-    
-    func makeUIViewController(context: Context) -> UIViewController {
-        return UIViewController() // 빈 UIViewController 반환
-    }
-
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-        // UIViewController 업데이트 로직 (필요시)
-    }
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-
-    class Coordinator: NSObject, GADFullScreenContentDelegate {
-        var parent: GADInterstitialView
-        var interstitial: GADInterstitialAd?
-
-        init(_ parent: GADInterstitialView) {
-            self.parent = parent
-            super.init()
-            loadInterstitialAd()
-        }
-
-        private func loadInterstitialAd() {
-            let request = GADRequest()
-            GADInterstitialAd.load(withAdUnitID: parent.adUnitID, request: request) { (ad, error) in
-                if let error = error {
-                    print("Failed to load interstitial ad with error: \(error.localizedDescription)")
-                    return
-                }
-                self.interstitial = ad
-                self.interstitial?.fullScreenContentDelegate = self
-            }
-        }
-
-        func showAd(from rootViewController: UIViewController) {
-            if let ad = interstitial {
-                ad.present(fromRootViewController: rootViewController)
-            } else {
-                print("Ad wasn't ready")
-            }
-        }
-
-        // MARK: - GADFullScreenContentDelegate methods
-
-          func adDidRecordImpression(_ ad: GADFullScreenPresentingAd) {
-            print("\(#function) called")
-          }
-
-          func adDidRecordClick(_ ad: GADFullScreenPresentingAd) {
-            print("\(#function) called")
-          }
-
-          func ad(_ ad: GADFullScreenPresentingAd, didFailToPresentFullScreenContentWithError error: Error) {
-            print("\(#function) called")
-          }
-
-          func adWillPresentFullScreenContent(_ ad: GADFullScreenPresentingAd) {
-            print("\(#function) called")
-          }
-
-          func adWillDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
-            print("\(#function) called")
-          }
-
-          func adDidDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
-            print("\(#function) called")
-          }
-    }
-}
-
 class AdCoordinator: NSObject, GADFullScreenContentDelegate {
     var interstitial: GADInterstitialAd?
     var onAdDismiss: (() -> Void)?
@@ -91,7 +17,6 @@ class AdCoordinator: NSObject, GADFullScreenContentDelegate {
         super.init()
         loadInterstitialAd()
     }
-
     private func loadInterstitialAd() {
         let request = GADRequest()
         var adUnitID: String = "ca-app-pub-3940256099942544/5135589807"
